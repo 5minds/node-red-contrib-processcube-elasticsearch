06a75c52b3b9909ba57f637241e37831ffab82a3
Martin Möllenbeck <martin.moellenbeck@5minds.de>
2025-01-21 17:47:14 +0100

    Structur code and add handling of reject and unhandled promise from winston-elasticsearch

--- elastic-search-logger.js
+++ elastic-search-logger.js
@@ -13,9 +13,9 @@ const logLevels = {
     }
 };
 
-function setElasticFields(logData) {
+function transformElasticFields(logData) {
     const transformed = ElasticsearchTransformer(logData);
-    
+
     transformed['@timestamp'] = logData.timestamp ? logData.timestamp : new Date().toISOString();
     transformed.message = logData.message;
     transformed.messageTemplate = logData.messageTemplate;
@@ -28,19 +28,35 @@ function setElasticFields(logData) {
     }
 
     if (logData.meta['trace.id']) {
-        transformed.trace = { 
-            id: logData.meta['trace.id'] 
+        transformed.trace = {
+            id: logData.meta['trace.id']
         };
     }
 
     if (logData.meta['span.id']) {
-        transformed.span = { 
+        transformed.span = {
             id: logData.meta['span.id']
         };
     }
 
     return transformed;
 }
+
+function raiseErrorAndSetNodeStatus(node, message) {
+    node.error(message, {});
+    node.status({
+        fill: 'red',
+        shape: 'ring',
+        text: message,
+    });
+}
+
+
+// TODO: MM winston-elasticsearch/index.js#112 wird die log als async function deklariert, 
+// aber nicht als async aufgerufen
+process.on('unhandledRejection', (reason, promise) => {
+    console.error(`Unhandled Rejection at ${promise} reason: ${reason}`, {});
+});    
 
 
 module.exports = function (RED) {
@@ -53,30 +69,29 @@ module.exports = function (RED) {
 
         // Elastic settings
         const url = RED.util.evaluateNodeProperty(config.url, config.urlType, node);
-        if (url == '') {
-            node.error('Elastic search url is not set', {});
+        if (!url || url === 'undefined') {
+            raiseErrorAndSetNodeStatus(node, 'Elastic search url is not set');
         }
 
         const user = RED.util.evaluateNodeProperty(this.credentials.username, config.usernameType, node);
-        if (user == '') {
-            node.error('Elastic search username is not set', {});
+        if (!user || user === 'undefined') {
+            raiseErrorAndSetNodeStatus(node, 'Elastic search username is not set');
         }
 
         const password = RED.util.evaluateNodeProperty(this.credentials.password, config.passwordType, node);
-        if (password == '') {
-            node.error('Elastic search password is not set', {});
+        if (!password || password === 'undefined') {
+            raiseErrorAndSetNodeStatus(node, 'Elastic search password is not set');
         }
 
         let index = RED.util.evaluateNodeProperty(this.credentials.index, config.indexType, node);
-        if (index == '') {
-            node.error('Elastic search index is not set', {});
+        if (!index || index === 'undefined') {
+            raiseErrorAndSetNodeStatus(node, 'Elastic search index is not set');
         } else {
             index = index.toLowerCase();
         }
 
-        let transports = [];
-
         if (url) {
+            const transports = [];
             const elasticSearchTransport = new winstonElasticSearch.ElasticsearchTransport({
                 clientOpts: {
                     node: url,
@@ -89,35 +104,30 @@ module.exports = function (RED) {
                         rejectUnauthorized: false,
                     },
                 },
-                transformer: (logData) => {
-                    try {
-                        setElasticFields(logData);
-                    } catch (error) {
-                        node.error(error, {});
-                    }
-                },
+                transformer: (logData) => transformElasticFields(logData),
                 index: index,
             });
 
             transports.push(elasticSearchTransport);
 
             elasticSearchTransport.on('error', (error) => {
-                node.error(`Error in elasticSearchTransport caught: ${error.message}`, {});                
+                raiseErrorAndSetNodeStatus(node, `Error in elasticSearchTransport caught: ${error.message}`);
             });
+
+            node.logger = new winston.createLogger({
+                exitOnError: false,
+                level: 'Debug',
+                levels: logLevels.levels,
+                transports: transports,
+            });
+
+            node.logger.on('error', (error) => {
+                raiseErrorAndSetNodeStatus(node, `Error in logger caught: ${error.message}`);
+            });
+
+            this.debug('elastic-search logger created');
         }
 
-        this.logger = new winston.createLogger({
-            exitOnError: false,
-            level: 'Debug',
-            levels: logLevels.levels,
-            transports: transports,
-        });
-
-        this.logger.on('error', (error) => {
-            node.error(error, {});
-        });
-
-        this.debug('elastic-search logger created');
 
         this.on('close', function () {
             // close logger
@@ -131,7 +141,11 @@ module.exports = function (RED) {
 
     LogElasticLoggerNode.prototype.addToLog = function addTolog(loglevel, msg) {
         try {
-            this.logger.log(loglevel, msg.payload.message, msg.payload.meta);
+            if (this.logger == null) {
+                raiseErrorAndSetNodeStatus(this, `Elastic search logger is not initialized: ${JSON.stringify(msg)}`);
+            } else {
+                this.logger.log(loglevel, msg.payload.message, msg.payload.meta);
+            }
         } catch (error) {
             this.error(error, msg);
         }
